877afb013da27b8a537b1f1d20b18666746ede43
Grace <149479620+Grace0912YuYa@users.noreply.github.com>
2025-05-11 20:47:15 +0800

    Update main.dart
    include the register and login file into main.dart

--- settle_up/lib/main.dart
+++ settle_up/lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'register_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Auth UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF0F3F33),
+          primary: const Color(0xFF0F3F33),
         ),
+        useMaterial3: true,
       ),
+      home: const LoginPage(), // Start with login page
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+      },
     );
   }
 }
